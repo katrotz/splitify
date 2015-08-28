aa4c320f6b3495b9e4b5996f064c68aa9513e377
katrotz <vcotruta@pycoding.biz>
2015-08-28 16:13:02 +0300

    Structure change

--- client/init.js
+++
@@ -1,7 +0,0 @@
-Meteor.startup(function() {
-  // Potentially prompts the user to enable location services. We do this early
-  // on in order to have the most accurate location by the time the user shares
-  Geolocation.currentLocation();
-
-  console.log(Package['snapsvg:core'].Snap);
-});
---
+++ client/view/restaurant/restaurant-list.js
@@ -0,0 +1,1 @@
+console.log('RESTAURANT LIST');
